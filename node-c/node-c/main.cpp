38a6b47f35134dad4813233eef1a4e62a29a9ebe
guobin <844922881@qq.com>
2019-05-25 11:32:17 +0800

    add
    xcode config

---
+++ node-c/node-c/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  node-c
+//
+//  Created by 郭斌 on 2019/5/25.
+//  Copyright © 2019 guobin. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
