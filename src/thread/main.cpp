321a0e85bed0cd5b660965b0d59450a5ec3f2907
guobin <844922881@qq.com>
2019-05-28 13:29:12 +0800

    array 的长度

--- src/thread/main.cpp
+++ src/thread/main.cpp
@@ -9,9 +9,20 @@
 #include <iostream>
 
 using namespace std;
+void sumOf(float a)
+{
+    cout << a << endl;
+}
+void sumOf(float a, float b)
+{
+    cout << a + b << endl;
+}
 
 int main(int argc, const char *argv[])
 {
-
+    sumOf(5.5);
+    sumOf(1, 2.2);
+    float arr[] = {1, 2.5, 3.5, 4};
+    cout << sizeof(arr) / sizeof(arr[0])<< endl;
     return 0;
 }
