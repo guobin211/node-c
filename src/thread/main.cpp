cfba7bf3059d5038ffc9f4016f7e4ac1933e0e2c
GuoBin <844922881@qq.com>
2019-06-05 21:56:09 +0800

    basic

--- src/thread/main.cpp
+++ src/thread/main.cpp
@@ -16,6 +16,35 @@ void hello(int n) {
 }
 
 int main(int argc, const char *argv[]) {
+    // 计算机最小的单位 bit 0或者1组成的2进制
+    // 1byte = 8bit 0000 0000
+    // tb = 1024gb = 1024mb = 1024kb = 1024byte = 8bit
+    bool success;
+    char firstChar;
+    int rating;
+    float ratingF;
+    double ratingD;
+    wchar_t wtChar;
+    // 1 byte
+    cout << sizeof(success) << endl;
+    // 1 byte = 8bit  -128~127 或者
+    cout << sizeof(firstChar) << endl;
+    // 4 byte 
+    cout << sizeof(rating) << endl;
+     // 4 byte
+    cout << sizeof(ratingF) << endl;
+     // 8 byte
+    cout << sizeof(ratingD) << endl;
+     // 4 byte
+    cout << sizeof(wtChar) << endl;
+    // 8 byte long修饰符 长整形
+    long int la = 9223372036854775807;
+    // 16 byte 长double
+    long double ld = 16;
+    cout << la << endl;
+    cout << sizeof(la) << endl;
+    cout << sizeof(ld) << endl;
+
     sumOf(5.5);
     sumOf(1, 2.2);
     float arr[] = {1, 2.5, 3.5, 4};
@@ -27,5 +56,9 @@ int main(int argc, const char *argv[]) {
     a = a - b;
     cout << a << b << endl;
     hello(3);
+    // 枚举类
+    enum color {red, blue=5, green} colorEnum;
+    colorEnum=blue;
+    cout << colorEnum << endl;
     return 0;
 }
