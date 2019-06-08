7795fd0b4788b9e635b0f45cdbeaf1fd5211fa74
GuoBin <844922881@qq.com>
2019-06-08 10:07:38 +0800

    file

---
+++ node/io/flie.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const Path = require('../utils/path');
+const path = new Path();
+const stream = require('stream');
+
+console.log(__dirname);
+console.log(__filename);
+console.log(path.getDir());
+
---
+++ node/utils/path.js
@@ -0,0 +1,5 @@
+module.exports = class Path {
+    getDir() {
+        return __dirname;
+    }
+}
